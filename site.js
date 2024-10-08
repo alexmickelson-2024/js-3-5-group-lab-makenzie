5a5ddbe9f0ebcb8bc8721025c753d1bbf96dd586
Makenzie Orme <makenzie.orme@students.snow.edu>
2024-10-08 12:19:10 -0600

    javascript is added

--- site.js
+++ site.js
@@ -1,17 +1,33 @@
 import { bookList } from "./books.js";
 
 
-const drawBooks = (books) => {
-  const bookHTML = (book) => `
-    <tr>
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.summary}</td>
-    </tr>
-  `
-
+// const drawBooks = (books) => {
+//   const bookHTML = (book) => `
+//     <tr>
+//       <td>${book.title}</td>
+//       <td>${book.author}</td>
+//       <td>${book.summary}</td>
+//     </tr>
+//   `
+// }
+function drawBooks(books){
   const tableBodyElement = document.getElementById("myTableBody")
-  tableBodyElement.innerHTML = "";
+  books.forEach((book) => {
+    const rowElement = document.createElement("tr")
+    const titleElement = document.createElement("td");
+    const authorElement = document.createElement("td");
+    const summaryElement = document.createElement("td");
+    titleElement.textContent = book.title;
+    summaryElement.textContent = book.summary;
+    authorElement.textContent = book.author;
+    rowElement.addEventListener(("click"),(book) =>{
+      console.log("book has been clicked")
+    })
+    rowElement.appendChild(titleElement);
+    rowElement.appendChild(authorElement);
+    rowElement.appendChild(summaryElement);
+    tableBodyElement.appendChild(rowElement);
+  });
 
   books.forEach(book => {
     const firstBookHtml = bookHTML(book)
