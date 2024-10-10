b3fc26ace6de160bfa0a796342fde2e9ad7f2523
Makenzie Orme <makenzie.orme@students.snow.edu>
2024-10-10 12:22:20 -0600

    added event listeners

--- site.js
+++ site.js
@@ -1,6 +1,5 @@
 import { bookList } from "./books.js";
 
-
 // const drawBooks = (books) => {
 //   const bookHTML = (book) => `
 //     <tr>
@@ -10,29 +9,61 @@ import { bookList } from "./books.js";
 //     </tr>
 //   `
 // }
-function drawBooks(books){
-  const tableBodyElement = document.getElementById("myTableBody")
+var newList = [...bookList];
+function drawBooks(books) {
+  const tableBodyElement = document.getElementById("myTableBody");
+  tableBodyElement.textContent = "";
   books.forEach((book) => {
-    const rowElement = document.createElement("tr")
+    const rowElement = document.createElement("tr");
     const titleElement = document.createElement("td");
     const authorElement = document.createElement("td");
     const summaryElement = document.createElement("td");
     titleElement.textContent = book.title;
     summaryElement.textContent = book.summary;
     authorElement.textContent = book.author;
-    rowElement.addEventListener(("click"),(book) =>{
-      console.log("book has been clicked")
-    })
     rowElement.appendChild(titleElement);
     rowElement.appendChild(authorElement);
     rowElement.appendChild(summaryElement);
+    rowElement.addEventListener("click", (clickBook) => {
+      console.log(clickBook, book);
+      const titleofBook = document.createElement("p");
+      const bookClicked = document.getElementById("selectedBookTitle");
+      bookClicked.textContent = "";
+      titleofBook.textContent = book.title;
+      bookClicked.appendChild(titleofBook);
+    });
     tableBodyElement.appendChild(rowElement);
   });
 
-  books.forEach(book => {
-    const firstBookHtml = bookHTML(book)
-    tableBodyElement.innerHTML += firstBookHtml;
-  })
+  // books.forEach(book => {
+  //   const firstBookHtml = bookHTML(book)
+  //   tableBodyElement.innerHTML += firstBookHtml;
+  // })
 }
 
-drawBooks(bookList);
+function FilterBooks() {
+  const filterInput = document.getElementById("filterInput");
+  filterInput.addEventListener("input", (inputEvent) => {
+    const keyword = inputEvent.target.value.toLowerCase();
+    filterbook()
+
+  function filterbook() {
+    newList = bookList.filter((filteredBook) =>
+      filterTitleDescriptionandAuthor(filteredBook, keyword)
+    );
+  }
+  function filterTitleDescriptionandAuthor(book, keyword) {
+    return (
+      (book.summary).toLowerCase().includes(keyword) ||
+      (book.title).toLowerCase().includes(keyword) ||
+      (book.author).toLowerCase().includes(keyword)
+    );
+  }
+  console.log(newList)
+  return newList;
+})
+}
+
+FilterBooks();
+console.log(newList)
+drawBooks(newList);
